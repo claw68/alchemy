02e3665d2569a4174f3e958c8f306c794798c4d1
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-11-25 14:51:28 +0800

    Add content to dashboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,12 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Dashboard extends CI_Controller 
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
 	public function index()
 	{
-		$this->load->view('dashboard');
+		$data = new stdClass();
+		$navigation = navigation();
+		render_layout('dashboard/view', $data, $navigation);
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file dashboard.php */
+/* Location: ./application/controllers/dashboard.php */
