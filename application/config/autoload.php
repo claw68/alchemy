aa78602943a528053cbbbea998c775faa26ab94d
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-11-25 14:44:01 +0800

    Update codeigniter config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'file', 'text', 'security');
+$autoload['helper'] = array('url', 'file', 'text', 'security', 'render', 'navigation');
 
 
 /*
