6fc0f0dbabbb50cee08997e900495164e7ba9a1e
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-11-25 12:11:13 +0800

    Add temporary main js and css

---
+++ includes/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+	$(".add").button();
+	$(".edit").button();
+	$(".delete").button().click(function(){
+		var res = confirm("Are you sure you want to delete this item?");
+		if(res)
+		window.location.href=$(this).attr('href');
+		else
+		return false;
+	});
+});
