95280ba3bc27047b215d03202581f7499917a4ef
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-11-26 18:30:40 +0800

    Update database

--- template.sql
+++ template.sql
@@ -44,6 +44,18 @@ CREATE TABLE `demo` (
 
 /*Data for the table `demo` */
 
+/*Table structure for table `gallery` */
+
+DROP TABLE IF EXISTS `gallery`;
+
+CREATE TABLE `gallery` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `desc` varchar(255) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 /*Table structure for table `login_attempts` */
 
 DROP TABLE IF EXISTS `login_attempts`;
@@ -106,7 +118,7 @@ CREATE TABLE `user_profiles` (
   `user_id` int(11) NOT NULL,
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `user_profiles` */
 
@@ -137,7 +149,7 @@ CREATE TABLE `users` (
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`username`,`password`,`email`,`activated`,`banned`,`ban_reason`,`new_password_key`,`new_password_requested`,`new_email`,`new_email_key`,`last_ip`,`last_login`,`created`,`modified`) values (1,'admin','$2a$08$3GqdJ0PRvk5axGx/vWRQiuH5NeU9TAUY0qbhBNeWo501XPCFb9BX2','franzandrew68@gmail.com',1,0,NULL,NULL,NULL,NULL,NULL,'::1','2013-11-26 16:48:29','2013-11-26 16:10:31','2013-11-26 16:48:29');
+insert  into `users`(`id`,`username`,`password`,`email`,`activated`,`banned`,`ban_reason`,`new_password_key`,`new_password_requested`,`new_email`,`new_email_key`,`last_ip`,`last_login`,`created`,`modified`) values (1,'admin','$2a$08$3GqdJ0PRvk5axGx/vWRQiuH5NeU9TAUY0qbhBNeWo501XPCFb9BX2','franzandrew68@gmail.com',1,0,NULL,NULL,NULL,NULL,NULL,'::1','2013-11-26 18:17:47','2013-11-26 16:10:31','2013-11-26 18:17:47');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
