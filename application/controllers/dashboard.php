b24dcefce3f7392f55fdc237a82b754b32f60684
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-11-26 16:47:07 +0800

    Add edit profile page

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -23,6 +23,27 @@ class Dashboard extends CI_Controller
 		$navigation = navigation();
 		render_layout('dashboard/view', $data, $navigation);
 	}
+	
+	public function profile()
+	{
+		$data = new stdClass();
+		$user_id = $this->tank_auth->get_user_id();
+		
+		$data->username = $this->tank_auth->get_username();
+		$data->profile = $this->users->get_profile_by_user_id($user_id);
+		
+		$navigation = navigation();
+		render_layout('dashboard/profile', $data, $navigation);
+	}
+	
+	public function doProfile()
+	{
+		$user_id = $this->tank_auth->get_user_id();
+		
+		$post = $this->input->post();
+		$this->users->update_profile($user_id, $post);
+		redirect('/dashboard');
+	}
 }
 
 /* End of file dashboard.php */
