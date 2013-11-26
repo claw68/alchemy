1a78fc14c52ccf86e4605475a588d69169cf8306
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-11-26 18:03:44 +0800

    Add basic CRUD for gallery

---
+++ application/controllers/gallery.php
@@ -0,0 +1,66 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Gallery extends CI_Controller 
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('gallery_m', 'gallery');
+		
+		if (!$this->tank_auth->is_logged_in()) {
+			redirect('/auth/login/');
+		}
+	}
+
+	public function index()
+	{
+		$data = new stdClass();
+		$data->gallery = $this->gallery->all();
+		
+		$navigation = navigation();
+		render_layout('gallery/list', $data, $navigation);
+	}
+	
+	function add()
+	{
+		$data = new stdClass();
+		
+		$navigation = navigation();
+		render_layout('gallery/add', $data, $navigation);
+	}
+	
+	function doAdd()
+	{
+		$post = $this->input->post();
+		$this->gallery->add($post);
+		redirect('/gallery');
+	}
+	
+	function edit($id)
+	{
+		$data = new stdClass();
+		$data->id = $id;
+		$data->gallery = $this->gallery->get($id);
+		if(!$data->gallery)
+			redirect('/gallery');
+		
+		$navigation = navigation();
+		render_layout('gallery/edit', $data, $navigation);
+	}
+	
+	function doEdit()
+	{
+		$post = $this->input->post();
+		$this->gallery->update($post["id"], $post);
+		redirect('/gallery');
+	}
+	
+	function delete($id)
+	{
+		$this->gallery->delete($id);
+		redirect('/gallery');
+	}
+}
+
+/* End of file gallery.php */
+/* Location: ./application/controllers/gallery.php */
