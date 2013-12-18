8171ba5472a6899fb121a7e65005c9fc9ef0d681
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-12-18 15:46:01 +0800

    Add to navigation

--- application/helpers/navigation_helper.php
+++ application/helpers/navigation_helper.php
@@ -4,6 +4,7 @@
 		$navigation = Array();
 		$navigation[] = (object) Array('url' => 'dashboard', 'label' => 'Dashboard');
 		$navigation[] = (object) Array('url' => 'dashboard/profile', 'label' => 'Profile');
+		$navigation[] = (object) Array('url' => 'generate', 'label' => 'Generate');
 		$navigation[] = (object) Array('url' => 'demo', 'label' => 'Demo');
 		$navigation[] = (object) Array('url' => 'gallery', 'label' => 'Gallery');
 		$navigation[] = (object) Array('url' => 'auth/logout', 'label' => 'Log out');
