7615841d2408e8acddff7fac9e6759c57ca0ad3f
Franz Andrew Vallente <franzandrew68@gmail.com>
2013-12-20 15:50:41 +0800

    Update dashboard test

--- tests/controllers/DashboardTest.php
+++ tests/controllers/DashboardTest.php
@@ -10,19 +10,30 @@ class DashboardTest extends CIUnit_TestCase
 	{
 		// Set the tested controller
 		$this->CI = set_controller('dashboard');
+		$this->CI->session->set_userdata('user_id','1');
+		$this->CI->session->set_userdata('username','admin');
 	}
 	
 	public function testIndex()
 	{
-		$this->CI->session->set_userdata('user_id','1');
-		$this->CI->session->set_userdata('username','admin');
-		
 		// Call the controllers method
 		$this->CI->index();
 		
 		// Fetch the buffered output
 		$out = output();
 		
+		// Check if the content is OK
+		$this->assertSame(0, preg_match('/(error|notice)/i', $out));
+	}
+	
+	public function testProfile()
+	{
+		// Call the controllers method
+		$this->CI->profile();
+		
+		// Fetch the buffered output
+		$out = output();
+				
 		// Check if the content is OK
 		$this->assertSame(0, preg_match('/(error|notice)/i', $out));
 	}
