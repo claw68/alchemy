b2e7d90349f5c8eb85401ac966dc3acf1a90aca7
Franz Andrew Vallente <franzandrew68@gmail.com>
2014-01-22 16:01:12 +0800

    Add generate mvc controller function

--- application/controllers/generate.php
+++ application/controllers/generate.php
@@ -18,6 +18,47 @@ class Generate extends CI_Controller
 		$navigation = navigation();
 		render_layout('generate/view', $data, $navigation);
 	}
+	
+	public function doGenerate()
+	{
+		$object = $this->input->post('object');
+		
+		$template = 'resource/template/controller.php';
+		$target = 'application/controllers/'.$object.'.php';
+		$this->generateByTemplate($template, $object, $target);
+		
+		$template = 'resource/template/model.php';
+		$target = 'application/models/'.$object.'_m.php';
+		$this->generateByTemplate($template, $object, $target);
+		
+		$target_dir = 'application/views/'.$object.'/';
+		if (!file_exists($target_dir)) 
+   			mkdir($target_dir, 0755, TRUE);
+		
+		$template = 'resource/template/views/add.php';
+		$this->generateByTemplate($template, $object, $target_dir.'add.php');
+		$template = 'resource/template/views/edit.php';
+		$this->generateByTemplate($template, $object, $target_dir.'edit.php');
+		$template = 'resource/template/views/list.php';
+		$this->generateByTemplate($template, $object, $target_dir.'list.php');
+	}
+	
+	private function generateByTemplate($template, $object, $target)
+	{
+		$file = read_file($template);
+		
+		echo $file;
+		
+		$find = Array('__Object', '__object');
+		$replace = Array(ucfirst($object), $object);
+		
+		$file = str_replace($find, $replace, $file);
+		
+		write_file($target, $file);
+		
+		return $target;
+	}
+	
 }
 
 /* End of file generate.php */
