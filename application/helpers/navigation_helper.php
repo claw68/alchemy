602504e83c12e4f14dd45c8e7cb4ca85ec7f2d82
Franz Andrew Vallente <franzandrew68@gmail.com>
2014-01-22 17:57:35 +0800

    Remove generate in navigation

--- application/helpers/navigation_helper.php
+++ application/helpers/navigation_helper.php
@@ -4,7 +4,6 @@
 		$navigation = Array();
 		$navigation[] = (object) Array('url' => 'dashboard', 'label' => 'Dashboard');
 		$navigation[] = (object) Array('url' => 'dashboard/profile', 'label' => 'Profile');
-		$navigation[] = (object) Array('url' => 'generate', 'label' => 'Generate');
 		$navigation[] = (object) Array('url' => 'object', 'label' => 'Object');
 		$navigation[] = (object) Array('url' => 'demo', 'label' => 'Demo');
 		$navigation[] = (object) Array('url' => 'gallery', 'label' => 'Gallery');
