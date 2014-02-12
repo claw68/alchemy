937a81681686cbd09674c8bd714e067cded26215
Franz Andrew Vallente <franzandrew68@gmail.com>
2014-02-12 21:01:36 +0800

    Add flag to display user info if logged in

--- application/views/template/navigation.php
+++ application/views/template/navigation.php
@@ -13,10 +13,12 @@
 		<li><a href="<?php echo site_url().$nav->url; ?>"><?php echo $nav->label; ?></a></li>
 		<?php } ?>
 	</ul>
+	<?php if (isset($profile) && isset($username)) { ?>
 	<ul class="navigation" id="user-info">
 		<li>Logged in as: <a href="<?php echo site_url(); ?>dashboard/profile"><?php echo $username; ?></a></li>
 		<li>Hi, <strong><?php echo $profile->first_name." ".$profile->last_name; ?></strong>!</li>
 		<li><a href="<?php echo site_url(); ?>auth/logout">Logout</a></li>
 	</ul>
+	<?php } ?>
 	<div class="clearer"></div>
 </div>
