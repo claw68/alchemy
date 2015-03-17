1520867ede544519ff5dee75bbd57e7b87f4c9b3
claw68 <franzandrew68@gmail.com>
2015-03-17 20:17:31 +0800

    Edit table columns

--- alchemy.sql
+++ alchemy.sql
@@ -66,7 +66,9 @@ DROP TABLE IF EXISTS `effects_map`;
 
 CREATE TABLE `effects_map` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `ingredient` int(255) NOT NULL,
+  `effect` int(255) NOT NULL,
+  `position` int(255) NOT NULL,
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
