c424341220024056c2da43496aa503f874fc97a2
claw68 <franzandrew68@gmail.com>
2015-03-17 17:41:00 +0800

    Add ingredients object

---
+++ application/views/ingredients/add.php
@@ -0,0 +1,35 @@
+<div>
+	<div style="padding-bottom: 15px;">
+		<p style="font-weight: bold;">Add Ingredients</p>
+	</div>
+	<div style="padding-bottom: 20px;">
+		<a class="add" href='<?php echo site_url(); ?>ingredients'>Back to List</a>
+	</div>
+	<script>
+	function validate() {
+		var message  ="";
+		
+		if ($('#name').val() == '' || $('#name').val() == null) {
+			message += 'Name should have a value\n';
+		}
+		
+		if (message == "") {
+			return true;
+		} else {
+			alert(message);
+			return false;
+		}
+	}
+	</script>
+	<form onSubmit='return validate()' action="<?php echo site_url(); ?>ingredients/doAdd" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+		<table>
+			<tr>
+				<td><label for='name'>Name</label></td>
+				<td><input type='text' value='' id='name' name='name' /></td>
+			</tr>
+		</table>
+		<div style="padding-top: 15px;">
+			<input class="submit" type="submit" name="" value="Submit" />
+		</div>
+	</form>
+</div>
---
+++ application/views/ingredients/list.php
@@ -0,0 +1,25 @@
+<div>
+	<div style="padding-bottom: 15px;">
+		<p style="font-weight: bold;">List Ingredients</p>
+	</div>
+	<div style="padding-bottom: 20px;">
+		<a class="add" href='<?php echo site_url(); ?>ingredients/add'>Add ingredients</a>
+	</div>
+	<table class="list">
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Actions</th>
+		</tr>
+		<?php foreach ($ingredients as $row) { ?>
+			<tr>
+				<td><?php echo htmlspecialchars($row["id"], ENT_QUOTES); ?></td>
+				<td><?php echo htmlspecialchars($row["name"], ENT_QUOTES); ?></td>
+				<td>
+					<a class="delete" href="<?php echo site_url('')."ingredients/delete/".$row['id']; ?>">X</a>&nbsp;
+					<a class="edit" href="<?php echo site_url('')."ingredients/edit/".$row['id']; ?>">Edit</a>
+				</td>
+			</tr>
+		<?php } ?>
+	</table>
+</div>
