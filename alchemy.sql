d5f8e4cc780459b2548a1ab89b0e2a9c9aa2bc76
claw68 <franzandrew68@gmail.com>
2015-03-17 18:43:05 +0800

    Fix annoying git patch failed

--- alchemy.sql
+++ alchemy.sql
@@ -201,3 +201,4 @@ insert  into `users`(`id`,`username`,`password`,`email`,`activated`,`banned`,`ba
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
