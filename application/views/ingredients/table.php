ed9c76bdce0d14efd0a176a815a62886582de9aa
claw68 <franzandrew68@gmail.com>
2015-03-19 03:47:00 +0800

    Add foundation grid style to ingredients page

--- application/views/ingredients/table.php
+++ application/views/ingredients/table.php
@@ -1,23 +1,25 @@
-<div>
-	<div style="padding-bottom: 15px;">
+<div id="ingredients_table" class="row">
+	<div class="medium-12 columns" style="padding-bottom: 15px;">
 		<p style="font-weight: bold;">Ingredients Table</p>
 	</div>
-	<table class="list">
-		<tr>
-			<th>Ingredient</th>
-			<th>Primary</th>
-			<th>Secondary</th>
-			<th>Tertiary</th>
-			<th>Quarternary</th>
-		</tr>
-		<?php foreach ($ingredients as $row) { ?>
+	<div class="medium-12 columns">
+		<table class="list">
 			<tr>
-				<td><?php echo htmlspecialchars($row["ingredient"], ENT_QUOTES); ?></td>
-				<td><?php echo htmlspecialchars($row["primary"], ENT_QUOTES); ?></td>
-				<td><?php echo htmlspecialchars($row["secondary"], ENT_QUOTES); ?></td>
-				<td><?php echo htmlspecialchars($row["tertiary"], ENT_QUOTES); ?></td>
-				<td><?php echo htmlspecialchars($row["quarternary"], ENT_QUOTES); ?></td>
+				<th>Ingredient</th>
+				<th>Primary</th>
+				<th>Secondary</th>
+				<th>Tertiary</th>
+				<th>Quarternary</th>
 			</tr>
-		<?php } ?>
-	</table>
+			<?php foreach ($ingredients as $row) { ?>
+				<tr>
+					<td><?php echo htmlspecialchars($row["ingredient"], ENT_QUOTES); ?></td>
+					<td><?php echo htmlspecialchars($row["primary"], ENT_QUOTES); ?></td>
+					<td><?php echo htmlspecialchars($row["secondary"], ENT_QUOTES); ?></td>
+					<td><?php echo htmlspecialchars($row["tertiary"], ENT_QUOTES); ?></td>
+					<td><?php echo htmlspecialchars($row["quarternary"], ENT_QUOTES); ?></td>
+				</tr>
+			<?php } ?>
+		</table>
+	</div>
 </div>
