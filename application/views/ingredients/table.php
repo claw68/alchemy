bd5196b21068b7ddac7450b2c8a68f9141ab2b78
claw68 <franzandrew68@gmail.com>
2015-03-18 21:06:19 +0800

    Add table view

---
+++ application/views/ingredients/table.php
@@ -0,0 +1,29 @@
+<div>
+	<div style="padding-bottom: 15px;">
+		<p style="font-weight: bold;">Ingredients - Effects Table</p>
+	</div>
+	<table class="list">
+		<tr>
+			<th>ID</th>
+			<th>Ingredient</th>
+			<th>Primary</th>
+			<th>Secondary</th>
+			<th>Tertiary</th>
+			<th>Quarternary</th>
+			<th>Actions</th>
+		</tr>
+		<?php foreach ($ingredients as $row) { ?>
+			<tr>
+				<td><?php echo htmlspecialchars($row["id"], ENT_QUOTES); ?></td>
+				<td><?php echo htmlspecialchars($row["ingredient"], ENT_QUOTES); ?></td>
+				<td><?php echo htmlspecialchars($row["primary"], ENT_QUOTES); ?></td>
+				<td><?php echo htmlspecialchars($row["secondary"], ENT_QUOTES); ?></td>
+				<td><?php echo htmlspecialchars($row["tertiary"], ENT_QUOTES); ?></td>
+				<td><?php echo htmlspecialchars($row["quarternary"], ENT_QUOTES); ?></td>
+				<td>
+					<a href="#">View Compatible Ingredients</a>
+				</td>
+			</tr>
+		<?php } ?>
+	</table>
+</div>
