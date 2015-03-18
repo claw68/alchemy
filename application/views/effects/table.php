bd42b6cd51576ecafbc1a78306a8585c1dca1993
claw68 <franzandrew68@gmail.com>
2015-03-19 03:43:28 +0800

    Add foundation grid style to effects page

--- application/views/effects/table.php
+++ application/views/effects/table.php
@@ -1,38 +1,42 @@
-<div>
-	<div style="padding-bottom: 15px;">
+<div id="effects_table" class="row">
+	<div class="medium-12 columns" style="padding-bottom: 15px;">
 		<p style="font-weight: bold;">Effects Table</p>
 	</div>
-	<table class="list first">
-		<tr>
-			<th>Effect</th>
-			<th>Ingredients</th>
-		</tr>
-		<?php $break = 29; ?>
-		<?php foreach (array_slice ($effects, 0, $break) as $row) { ?>
+	<div class="medium-6 columns">
+		<table class="list">
 			<tr>
-				<td><?php echo htmlspecialchars($row["name"], ENT_QUOTES); ?></td>
-				<td>
-					<?php foreach ($row['ingredients'] as $col) { ?>
-						<?php echo htmlspecialchars($col["name"], ENT_QUOTES); ?><br />
-					<?php } ?>
-				</td>
+				<th>Effect</th>
+				<th>Ingredients</th>
 			</tr>
-		<?php } ?>
-	</table>
-	<table class="list">
-		<tr>
-			<th>Effect</th>
-			<th>Ingredients</th>
-		</tr>
-		<?php foreach (array_slice ($effects, $break, sizeof($effects)) as $row) { ?>
+			<?php $break = 29; ?>
+			<?php foreach (array_slice ($effects, 0, $break) as $row) { ?>
+				<tr>
+					<td><?php echo htmlspecialchars($row["name"], ENT_QUOTES); ?></td>
+					<td>
+						<?php foreach ($row['ingredients'] as $col) { ?>
+							<?php echo htmlspecialchars($col["name"], ENT_QUOTES); ?><br />
+						<?php } ?>
+					</td>
+				</tr>
+			<?php } ?>
+		</table>
+	</div>
+	<div class="medium-6 columns">
+		<table class="list">
 			<tr>
-				<td><?php echo htmlspecialchars($row["name"], ENT_QUOTES); ?></td>
-				<td>
-					<?php foreach ($row['ingredients'] as $col) { ?>
-						<?php echo htmlspecialchars($col["name"], ENT_QUOTES); ?><br />
-					<?php } ?>
-				</td>
+				<th>Effect</th>
+				<th>Ingredients</th>
 			</tr>
-		<?php } ?>
-	</table>
+			<?php foreach (array_slice ($effects, $break, sizeof($effects)) as $row) { ?>
+				<tr>
+					<td><?php echo htmlspecialchars($row["name"], ENT_QUOTES); ?></td>
+					<td>
+						<?php foreach ($row['ingredients'] as $col) { ?>
+							<?php echo htmlspecialchars($col["name"], ENT_QUOTES); ?><br />
+						<?php } ?>
+					</td>
+				</tr>
+			<?php } ?>
+		</table>
+	</div>
 </div>
