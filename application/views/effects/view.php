603b28c9d0c8ba12fd110de79cead2711fc41581
claw68 <franzandrew68@gmail.com>
2015-03-19 04:09:16 +0800

    Add view effect file

---
+++ application/views/effects/view.php
@@ -0,0 +1,19 @@
+<div id="effects_view" class="row">
+	<div class="medium-12 columns">
+		<p style="font-weight: bold;">Effect Information</p>
+	</div>
+	<div class="medium-12 columns">
+		<table>
+			<tr>
+				<th><?php echo $effects['name']; ?></th>
+			</tr>
+			<tr>
+				<td>
+					<?php foreach ($ingredients as $row) { ?>
+						<?php echo $row['name']; ?><br />
+					<?php } ?>
+				</td>
+			</tr>
+		</table>
+	</div>
+</div>
