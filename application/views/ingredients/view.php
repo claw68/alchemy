f20bee22c0907e4b1dad1729f339cd9137d96d2a
claw68 <franzandrew68@gmail.com>
2015-03-19 05:57:22 +0800

    Add ingredients view file

---
+++ application/views/ingredients/view.php
@@ -0,0 +1,21 @@
+<div id="ingredients_view" class="row">
+	<div class="medium-12 columns">
+		<p style="font-weight: bold;">Ingredients Effect List</p>
+	</div>
+	<div class="medium-12 columns">
+		<?php foreach ($effects as $effect) { ?>
+			<table>
+				<tr>
+					<th><?php echo $effect['name']; ?></th>
+				</tr>
+				<tr>
+					<td>
+						<?php foreach ($effect['ingredients'] as $row) { ?>
+							<?php echo $row['name']; ?><br />
+						<?php } ?>
+					</td>
+				</tr>
+			</table>
+		<?php } ?>
+	</div>
+</div>
