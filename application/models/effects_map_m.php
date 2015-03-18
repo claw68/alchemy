d13cfc234102450aea9f438b2500d12eefa6607a
claw68 <franzandrew68@gmail.com>
2015-03-18 21:06:00 +0800

    Add ingredients table query

--- application/models/effects_map_m.php
+++ application/models/effects_map_m.php
@@ -41,6 +41,51 @@ class Effects_map_m extends CI_Model
 		return $results;
 	}
 	
+	function ingredients_table()
+	{
+		$sql = "
+			SELECT
+				i.id,
+				i.name AS ingredient,
+				ef1.`effect_name` AS `primary`,
+				ef2.`effect_name` AS secondary,
+				ef3.`effect_name` AS tertiary,
+				ef4.`effect_name` AS quarternary
+			FROM
+				ingredients i
+					LEFT JOIN (
+						SELECT 
+							ef.* , e.`name` AS effect_name 
+						FROM effects_map ef, effects e 
+						WHERE ef.`effect` = e.`id`) ef1 
+					ON i.`id` = ef1.`ingredient` AND ef1.`position` = 1
+					
+					LEFT JOIN (
+						SELECT
+							ef.* , e.`name` AS effect_name
+						FROM effects_map ef, effects e 
+						WHERE ef.`effect` = e.`id`) ef2
+						ON i.`id` = ef2.`ingredient` AND ef2.`position` = 2
+						
+					LEFT JOIN (
+						SELECT
+							ef.* , e.`name` AS effect_name 
+						FROM effects_map ef, effects e
+						WHERE ef.`effect` = e.`id`) ef3 
+						ON i.`id` = ef3.`ingredient` AND ef3.`position` = 3
+					
+					LEFT JOIN (
+						SELECT 
+							ef.* , e.`name` AS effect_name
+						FROM effects_map ef, effects e
+						WHERE ef.`effect` = e.`id`) ef4 
+						ON i.`id` = ef4.`ingredient` AND ef4.`position` = 4";
+						
+		$query =  $this->db->query($sql);
+		$results =  $query->result_array();
+		return $results;
+	}
+	
 	function update($id, $data)
 	{
 		$data = $this->_strip_keys($data);
