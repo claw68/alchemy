c09c1cf1597a343b4d07d5ee6534cbf098452779
claw68 <franzandrew68@gmail.com>
2015-03-21 22:29:46 +0800

    Add thead and tbody for ingredients table tablesort

--- application/views/ingredients/table.php
+++ application/views/ingredients/table.php
@@ -4,54 +4,58 @@
 	</div>
 	<div class="medium-12 columns">
 		<table class="list">
-			<tr>
-				<th>Ingredient</th>
-				<th>Primary</th>
-				<th>Secondary</th>
-				<th>Tertiary</th>
-				<th>Quarternary</th>
-				<th>Most Expensive</th>
-				<th>Total</th>
-			</tr>
-			<?php foreach ($ingredients as $row) { ?>
+			<thead>
 				<tr>
-					<td>
-						<a href="<?php echo site_url("ingredients/view/".$row['id']); ?>">
-							<?php echo htmlspecialchars($row["ingredient"], ENT_QUOTES); ?></td>
-						</a>
-					<td>
-						<a href="<?php echo site_url("effects/view/".$row['em1id']); ?>">
-							<?php echo htmlspecialchars($row["primary"], ENT_QUOTES); ?>
-						</a>
-					</td>
-					<td>
-						<a href="<?php echo site_url("effects/view/".$row['em2id']); ?>">
-							<?php echo htmlspecialchars($row["secondary"], ENT_QUOTES); ?>
-						</a>
-					</td>
-					<td>
-						<a href="<?php echo site_url("effects/view/".$row['em3id']); ?>">
-							<?php echo htmlspecialchars($row["tertiary"], ENT_QUOTES); ?>
-						</a>
-					</td>
-					<td>
-						<a href="<?php echo site_url("effects/view/".$row['em4id']); ?>">
-							<?php echo htmlspecialchars($row["quarternary"], ENT_QUOTES); ?>
-						</a>
-					</td>
-					<td>
-						<?php foreach($row['best'] as $key => $col) { ?>
-							<a href="<?php echo site_url("effects/view/".$col['eid']); ?>">
-								<?php echo htmlspecialchars($col["effect_name"], ENT_QUOTES); if($key < sizeof($row['best']) - 1) echo ", "; ?>
-							</a>
-						<?php } ?>
-						(<?php echo htmlspecialchars($row["max_price"], ENT_QUOTES);?>)
-					</td>
-					<td>
-						<?php echo htmlspecialchars($row["price_total"], ENT_QUOTES); ?>
-					</td>
+					<th>Ingredient</th>
+					<th>Primary</th>
+					<th>Secondary</th>
+					<th>Tertiary</th>
+					<th>Quarternary</th>
+					<th>Most Expensive</th>
+					<th>Total</th>
 				</tr>
-			<?php } ?>
+			</thead>
+			<tbody>
+				<?php foreach ($ingredients as $row) { ?>
+					<tr>
+						<td>
+							<a href="<?php echo site_url("ingredients/view/".$row['id']); ?>">
+								<?php echo htmlspecialchars($row["ingredient"], ENT_QUOTES); ?></td>
+							</a>
+						<td>
+							<a href="<?php echo site_url("effects/view/".$row['em1id']); ?>">
+								<?php echo htmlspecialchars($row["primary"], ENT_QUOTES); ?>
+							</a>
+						</td>
+						<td>
+							<a href="<?php echo site_url("effects/view/".$row['em2id']); ?>">
+								<?php echo htmlspecialchars($row["secondary"], ENT_QUOTES); ?>
+							</a>
+						</td>
+						<td>
+							<a href="<?php echo site_url("effects/view/".$row['em3id']); ?>">
+								<?php echo htmlspecialchars($row["tertiary"], ENT_QUOTES); ?>
+							</a>
+						</td>
+						<td>
+							<a href="<?php echo site_url("effects/view/".$row['em4id']); ?>">
+								<?php echo htmlspecialchars($row["quarternary"], ENT_QUOTES); ?>
+							</a>
+						</td>
+						<td>
+							<?php foreach($row['best'] as $key => $col) { ?>
+								<a href="<?php echo site_url("effects/view/".$col['eid']); ?>">
+									<?php echo htmlspecialchars($col["effect_name"], ENT_QUOTES); if($key < sizeof($row['best']) - 1) echo ", "; ?>
+								</a>
+							<?php } ?>
+							(<?php echo htmlspecialchars($row["max_price"], ENT_QUOTES);?>)
+						</td>
+						<td>
+							<?php echo htmlspecialchars($row["price_total"], ENT_QUOTES); ?>
+						</td>
+					</tr>
+				<?php } ?>
+			</tbody>
 		</table>
 	</div>
 </div>
