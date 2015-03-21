648864f3f93b5c9bf42ae5afeb5cb89d9347b5da
claw68 <franzandrew68@gmail.com>
2015-03-21 22:36:38 +0800

    Update effects table to have only one table for sorting

--- application/views/effects/table.php
+++ application/views/effects/table.php
@@ -2,42 +2,14 @@
 	<div class="page-title medium-12 columns">
 		<p>Effects Table</p>
 	</div>
-	<div class="medium-6 columns">
+	<div class="medium-12 columns">
 		<table class="list">
 			<tr>
 				<th>Effect</th>
 				<th>Price</th>
 				<th>Ingredients</th>
 			</tr>
-			<?php $break = 29; ?>
-			<?php foreach (array_slice ($effects, 0, $break) as $row) { ?>
-				<tr>
-					<td>
-						<a href="<?php echo site_url("effects/view/".$row['id']); ?>">
-							<?php echo htmlspecialchars($row["name"], ENT_QUOTES); ?>
-						</a>	
-					</td>
-					<td><?php echo htmlspecialchars($row["price"], ENT_QUOTES); ?></td>
-					<td>
-						<?php foreach ($row['ingredients'] as $col) { ?>
-							<a href="<?php echo site_url("ingredients/view/".$col['id']); ?>">
-								<?php echo htmlspecialchars($col["name"], ENT_QUOTES); ?>
-							</a>
-							<br />
-						<?php } ?>
-					</td>
-				</tr>
-			<?php } ?>
-		</table>
-	</div>
-	<div class="medium-6 columns">
-		<table class="list">
-			<tr>
-				<th>Effect</th>
-				<th>Price</th>
-				<th>Ingredients</th>
-			</tr>
-			<?php foreach (array_slice ($effects, $break, sizeof($effects)) as $row) { ?>
+			<?php foreach ($effects as $row) { ?>
 				<tr>
 					<td>
 						<a href="<?php echo site_url("effects/view/".$row['id']); ?>">
