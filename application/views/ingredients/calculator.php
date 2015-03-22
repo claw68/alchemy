c8b9986c7b8b4b57d884c7b42c176c4278156c90
claw68 <franzandrew68@gmail.com>
2015-03-23 04:00:10 +0800

    Add result table

--- application/views/ingredients/calculator.php
+++ application/views/ingredients/calculator.php
@@ -18,4 +18,34 @@
 			</tbody>
 		</table>
 	</div>
+	<div class="medium-8 columns">
+		<p>Selected Ingredients</p>
+		<table id="calc_selected">
+			<tr>
+				<th>Primary</th>
+				<th>Secondary</th>
+				<th>Tertiary</th>
+			</tr>
+			<tr>
+				<td>--</td>
+				<td>--</td>
+				<td>--</td>
+			</tr>
+		</table>
+		<p>Result</p>
+		<table id="calc_result">
+			<tr>
+				<th>Effects</th>
+				<th>Price</th>
+			</tr>
+			<tr>
+				<td>--</td>
+				<td>--</td>
+			</tr>
+			<tr>
+				<td>Total</td>
+				<td>--</td>
+			</tr>
+		</table>
+	</div>
 </div>
