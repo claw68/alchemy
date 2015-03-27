306b244cc2659865c39cd96d38355681930f1c22
claw68 <franzandrew68@gmail.com>
2015-03-27 23:01:21 +0800

    Fix view to loop to all ingredients level

--- application/views/ingredients/matrix.php
+++ application/views/ingredients/matrix.php
@@ -1,27 +1,27 @@
 <div id="ingredients_matrix" class="row">
 	<?php foreach ($ingredients as $key => $ingredient) { ?>
-		<div class="page-title medium-12 columns">
-			<p><?php echo $ingredient['name']; ?></p>
-		</div>
-		<div class="medium-12 columns">
-			<table>
-				<tr>
-					<th>Compatible Ingredients</th>
-					<th>Price</th>
-				</tr>
-				<?php foreach ($ingredient['secondary'] as $secondary) { ?>
+		<?php foreach ($ingredient['secondary'] as $secondary) { ?>
+			<div class="page-title medium-12 columns">
+				<p><?php echo $ingredient['name']; ?> + <?php echo $secondary['name']; ?></p>
+			</div>
+			<div class="medium-12 columns">
+				<table>
 					<tr>
-						<td>
-							<a href="<?php echo site_url("ingredients/view/".$secondary['id']); ?>">
-								<?php echo $secondary['name']; ?>
-							</a>
-						</td>
-						<td>
-							<?php echo $secondary['price']; ?>
-						</td>
+						<th>Ingredients</th>
+						<th>Price</th>
 					</tr>
-				<?php } ?>
-			</table>
-		</div>
+					<?php foreach ($secondary['tertiary'] as $tertiary) { ?>
+						<tr>
+							<td>
+								<?php echo $tertiary['name']; ?>
+							</td>
+							<td>
+								<?php echo $tertiary['price']; ?>
+							</td>
+						</tr>
+					<?php } ?>
+				</table>
+			</div>
+		<?php } ?>
 	<?php } ?>
 </div>
