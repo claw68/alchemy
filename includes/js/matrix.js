d3093c278fad091dc73e97af24ad3d68375236cd
claw68 <franzandrew68@gmail.com>
2015-04-06 20:08:52 +0800

    Add toggle table function

--- includes/js/matrix.js
+++ includes/js/matrix.js
@@ -10,3 +10,14 @@ function get_tertiary_table(row, primary, secondary) {
 		$(row).append(data);
 	});
 }
+
+function toggle_table(row, primary, secondary) {
+	var table = $(row).find('table');
+	var table_exist = table.length > 0;
+	
+	if(table_exist) {
+		table.remove();
+	} else {
+		get_tertiary_table(row, primary, secondary);
+	}
+}
