25674dd0c2818e15567f02dca7fbc48f6c051d93
claw68 <franzandrew68@gmail.com>
2015-04-06 18:50:12 +0800

    Remove tertiary ingredient table from view

--- application/views/ingredients/matrix.php
+++ application/views/ingredients/matrix.php
@@ -4,24 +4,6 @@
 			<div class="page-title medium-12 columns">
 				<p><?php echo $ingredient['name']; ?> + <?php echo $secondary['name']; ?></p>
 			</div>
-			<div class="medium-12 columns">
-				<table>
-					<tr>
-						<th>Ingredients</th>
-						<th>Price</th>
-					</tr>
-					<?php foreach ($secondary['tertiary'] as $tertiary) { ?>
-						<tr>
-							<td>
-								<?php echo $tertiary['name']; ?>
-							</td>
-							<td>
-								<?php echo $tertiary['price']; ?>
-							</td>
-						</tr>
-					<?php } ?>
-				</table>
-			</div>
 		<?php } ?>
 	<?php } ?>
 	<div class="medium-12 columns">
