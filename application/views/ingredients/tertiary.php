5c79d93c58dd93e4c99ecb4c26bb7adc856b726e
claw68 <franzandrew68@gmail.com>
2015-04-06 19:23:17 +0800

    Add preliminary view file for tertiary ingredient table

---
+++ application/views/ingredients/tertiary.php
@@ -0,0 +1,16 @@
+<table>
+	<tr>
+		<th>Ingredients</th>
+		<th>Price</th>
+	</tr>
+	<?php foreach ($tertiary as $row) { ?>
+		<tr>
+			<td>
+				<?php echo $row['name']; ?>
+			</td>
+			<td>
+				<?php echo $row['price']; ?>
+			</td>
+		</tr>
+	<?php } ?>
+</table>
