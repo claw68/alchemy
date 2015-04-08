aab4f40d3c76f00c42b8ec9df6da7a1db34f797e
claw68 <franzandrew68@gmail.com>
2015-04-09 03:02:19 +0800

    Add view file for best value ingredient

---
+++ application/views/ingredients/best_value.php
@@ -0,0 +1,20 @@
+<table class="list">
+	<tr>
+		<th colspan="3">Best Value Ingredients Combination with <?php echo $info['name']; ?></th>
+		<th>Sell Price</th>
+	</tr>
+	<?php foreach ($by_ingredient as $row) { ?>
+		<tr>
+			<?php foreach ($row as $key => $col) { ?>
+				<td>
+					<a href="<?php echo site_url("ingredients/view/".$col['id']); ?>">
+						<?php echo $col['name']; ?>
+					</a>
+				</td>
+			<?php if($key > 1) break; } ?>
+			<td>
+				<?php echo array_sum(array_column($row['result'], 'price')); ?>
+			</td>
+		</tr>
+	<?php } ?>
+</table>
